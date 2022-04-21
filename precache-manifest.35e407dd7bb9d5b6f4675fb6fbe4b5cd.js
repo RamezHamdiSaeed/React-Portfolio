5ea3dc7b4790375c58ec1aa2ecf2f38e80a72b21
ramez1212 <317170274@sha.edu.eg>
2022-04-21 18:19:17 +0200

    Updates

---
+++ precache-manifest.35e407dd7bb9d5b6f4675fb6fbe4b5cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "254ed4d1541057b1f07a",
+    "url": "/React-Portfolio/static/js/main.254ed4d1.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "254ed4d1541057b1f07a",
+    "url": "/React-Portfolio/static/css/main.6283d0eb.chunk.css"
+  },
+  {
+    "revision": "a551ab8305106638fee5a8a5aa625646",
+    "url": "/React-Portfolio/index.html"
+  }
+];
