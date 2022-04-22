ecc4182d9ff60740ccee632fe9dc0da33c4e6e32
ramez1212 <317170274@sha.edu.eg>
2022-04-22 15:16:30 +0200

    configure contactUs section

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -16,7 +16,7 @@ export default class ContactUs extends Component {
               <div className="widget">
                 <h4>Linked in : <a href='https://www.linkedin.com/in/ramez-hamdi-788026215/'>{resumeData.linkedinId}</a>
                 </h4>
-                <h4>CV : <a href='https://app.rezi.ai/s/0BDaoqjF9KJknORvA8ds'>Ramez-Hamdi-Saeed-CV</a></h4>
+                <h4>CV :  <a href='https://app.rezi.ai/s/0BDaoqjF9KJknORvA8ds'>Ramez-Hamdi-Saeed-CV</a></h4>
               </div>
             </aside>
           </div>
