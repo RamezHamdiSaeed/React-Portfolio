37d37a6f9adb9bb57058a6f024467cc1254aa660
ramez1212 <317170274@sha.edu.eg>
2022-04-22 14:07:31 +0200

    update contactUs section

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -14,9 +14,9 @@ export default class ContactUs extends Component {
           <div className="row">
             <aside className="eigth columns footer-widgets">
               <div className="widget">
-                <h4>Linked in : 
-                  <a href='https://www.linkedin.com/in/ramez-hamdi-788026215/'>{resumeData.linkedinId}</a>
+                <h4>Linked in : <a href='https://www.linkedin.com/in/ramez-hamdi-788026215/'>{resumeData.linkedinId}</a>
                 </h4>
+                <h4>CV : <a href='https://app.rezi.ai/s/0BDaoqjF9KJknORvA8ds'>Ramez-Hamdi-Saeed-CV</a></h4>
               </div>
             </aside>
           </div>
